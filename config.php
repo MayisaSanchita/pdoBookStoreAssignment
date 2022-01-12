8faec9780f82a8bb40d89bca85e0a3ab2e4dc921
Mayisa Sanchita <97615099+MayisaSanchita@users.noreply.github.com>
2022-01-13 00:00:54 +0600

    ok

--- config.php
+++ config.php
@@ -1,13 +1,6 @@
-<!-- <?php
+<?php
     $host = 'sql6.freesqldatabase.com';
     $dbname = 'sql6465175';
     $username = 'sql6465175';
     $password = 'r6DMLTuHrY';
-?> -->
-
-<?php
-    $host = 'localhost';
-    $dbname = 'AssignmentPDO';
-    $username = 'root';
-    $password = '';
 ?>
